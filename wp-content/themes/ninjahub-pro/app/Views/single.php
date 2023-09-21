9873848b4ae7cd18d8144be9924d66907b607fb3
Ahmed Gamal <ahmed.gamal@mitchdesgins.com>
2023-09-21 23:52:33 +0300

    archive and single (blog, faqs)

--- wp-content/themes/ninjahub-pro/app/Views/single.php
+++ wp-content/themes/ninjahub-pro/app/Views/single.php
@@ -15,8 +15,8 @@ get_header();
 
 Nh_Hooks::enqueue_style(Nh::_DOMAIN_NAME . '-public-style-home-landing', Nh_Hooks::PATHS['public']['css'] . '/pages/landing/home');
 global $post;
-$single = new Nh_Blog();
-$single = $single->convert($post);
+$blog_obj = new Nh_Blog();
+$single = $blog_obj->convert($post);
 ?>
 <div class="single-blog">
     <div class="cover-image">
@@ -27,6 +27,32 @@ $single = $single->convert($post);
     </div>
     <div class="content">
         <?= $single->content ?>
+    </div>
+    <div class="related slick-slider">
+        <h3><?= _e("Other blogs", "ninja") ?></h3>
+        <?php
+        $related = $blog_obj->get_all(['publish'], 10, 'rand', 'ASC', [$single->ID]);
+        if (!empty($related)) {
+            foreach ($related as $single_related) {
+        ?>
+                <div class="related-card">
+                    <a class="blog-item" href="<?= $single_related->link ?>">
+                        <div class="img">
+                            <img src="<?= $single_related->thumbnail ?>" alt="B2B" />
+                            <span class="dots"></span>
+                        </div>
+                        <div class="date">
+                            <p><?= date('F d, Y', strtotime($single_related->created_date)) ?></p>
+                        </div>
+                        <div class="content">
+                            <?= $single_related->excerpt ?>
+                        </div>
+                    </a>
+                </div>
+        <?php
+            }
+        }
+        ?>
     </div>
 </div>
 
