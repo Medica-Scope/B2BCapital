6d009748b8d613775088e6257442ffea48f36e6c
Khaled Fathy <kfathy194@gmail.com>
2023-09-26 22:33:30 +0300

    add copyrights element to login&footer templates

--- wp-content/themes/ninjahub-pro/templates/template-page-registration.php
+++ wp-content/themes/ninjahub-pro/templates/template-page-registration.php
@@ -56,129 +56,132 @@ Nh_Hooks::enqueue_style( Nh::_DOMAIN_NAME . '-public-style-register-landing', Nh
 				</div>
 			</div>
 			<?php
-	echo Nh_Forms::get_instance()
-		->create_form( [
-			'custom-html-1'      => [
-				'type'    => 'html',
-				'content' => '<div class="d-flex flex-row flex-wrap col-12">',
-				'order'   => 0,
-			],
-			'first_name'         => [
-				'class'       => 'form-field col-6 pr-3',
-				'type'        => 'text',
-				'label'       => __( 'First Name', 'ninja' ),
-				'name'        => 'first_name',
-				'required'    => TRUE,
-				'placeholder' => __( 'Enter your first name', 'ninja' ),
-				'order'       => 5,
-			],
-			'last_name'          => [
-				'class'       => 'form-field col-6 pl-3',
-				'type'        => 'text',
-				'label'       => __( 'Last Name', 'ninja' ),
-				'name'        => 'last_name',
-				'required'    => TRUE,
-				'placeholder' => __( 'Enter your last name', 'ninja' ),
-				'order'       => 10,
-			],
-			'phone_number'       => [
-				'class'       => 'form-field col-6 pr-3',
-				'type'        => 'text',
-				'label'       => __( 'Phone Number', 'ninja' ),
-				'name'        => 'phone_number',
-				'required'    => TRUE,
-				'placeholder' => __( 'Enter your phone number', 'ninja' ),
-				'order'       => 15,
-			],
-			'user_email'         => [
-				'class'       => 'form-field col-6 pl-3',
-				'type'        => 'email',
-				'label'       => __( 'Email', 'ninja' ),
-				'name'        => 'user_email',
-				'required'    => TRUE,
-				'placeholder' => __( 'Enter your email', 'ninja' ),
-				'order'       => 20,
-			],
-			'user_password'      => [
-				'class'       => 'form-field col-6 pr-3',
-				'type'        => 'password',
-				'label'       => __( 'Password', 'ninja' ),
-				'name'        => 'user_password',
-				'required'    => TRUE,
-				'placeholder' => __( 'Enter your password', 'ninja' ),
-				'before'      => '<i class="fa-sharp fa-solid fa-eye-slash showPassIcon" data-target ="#' . Nh::_DOMAIN_NAME . '_user_password"></i>',
-				'order'       => 25,
-			],
-			'confirm_password'   => [
-				'class'       => 'form-field col-6 pl-3',
-				'type'        => 'password',
-				'label'       => __( 'Confirm Password', 'ninja' ),
-				'name'        => 'confirm_password',
-				'required'    => TRUE,
-				'placeholder' => __( 'Enter your confirm password', 'ninja' ),
-				'before'      => '<i class="fa-sharp fa-solid fa-eye-slash showPassIcon" data-target ="#' . Nh::_DOMAIN_NAME . '_confirm_password"></i>',
-				'order'       => 30,
-			],
-			'user_type'          => [
-				'type'           => 'select',
-				'label'          => __( 'User Type', 'ninja' ),
-				'name'           => 'user_type',
-				'required'       => TRUE,
-				'placeholder'    => __( 'Enter your user type', 'ninja' ),
-				'options'        => [
-					'owner'    => __( 'I am Owner', 'ninja' ),
-					'investor' => __( 'I am Investor', 'ninja' ),
-				],
-				'default_option' => '',
-				'select_option'  => [ $user_type ],
-				'class'          => 'form-field col-6 pr-3',
-				'order'          => 35,
-			],
-			'verification_type'  => [
-				'type'           => 'select',
-				'label'          => __( 'Account Verification Type', 'ninja' ),
-				'name'           => 'verification_type',
-				'required'       => TRUE,
-				'placeholder'    => __( 'Enter your verification type', 'ninja' ),
-				'options'        => [
-					Nh_User::VERIFICATION_TYPES['email'] => __( 'Email', 'ninja' ),
-					Nh_User::VERIFICATION_TYPES['mobile'] => __( 'Phone Number', 'ninja' ),
-					Nh_User::VERIFICATION_TYPES['whatsapp'] => __( 'Whatsapp', 'ninja' ),
-				],
-				'default_option' => '',
-				'select_option'  => '',
-				'class'          => 'form-field col-6 pl-3',
-				'order'          => 40,
-			],
-			'custom-html-3'      => [
-				'type'    => 'html',
-				'content' => '</div>',
-				'order'   => 45,
-			],
-			'registration_nonce' => [
-				'class' => '',
-				'type'  => 'nonce',
-				'name'  => 'registration_nonce',
-				'value' => Nh::_DOMAIN_NAME . "_registration_form",
-				'order' => 50
-			],
-			'submit'             => [
-				'class'               => 'form-action bbc-btn btn-primary large apply',
-				'type'                => 'submit',
-				'value'               => __( 'Create Account', 'ninja' ),
-				'before'              => '',
-				'after'               => '',
-				'recaptcha_form_name' => 'frontend_registration',
-				'order'               => 55
-			],
-		], [
-			'class' => Nh::_DOMAIN_NAME . '-registration-form',
-			'id'    => Nh::_DOMAIN_NAME . '_registration_form'
-		] );
-        /** TODO!: create function to return copyrights */
-	?>
+			echo Nh_Forms::get_instance()
+				->create_form( [ 
+					'custom-html-1'      => [ 
+						'type'    => 'html',
+						'content' => '<div class="d-flex flex-row flex-wrap col-12">',
+						'order'   => 0,
+					],
+					'first_name'         => [ 
+						'class'       => 'form-field col-6 pr-3',
+						'type'        => 'text',
+						'label'       => __( 'First Name', 'ninja' ),
+						'name'        => 'first_name',
+						'required'    => TRUE,
+						'placeholder' => __( 'Enter your first name', 'ninja' ),
+						'order'       => 5,
+					],
+					'last_name'          => [ 
+						'class'       => 'form-field col-6 pl-3',
+						'type'        => 'text',
+						'label'       => __( 'Last Name', 'ninja' ),
+						'name'        => 'last_name',
+						'required'    => TRUE,
+						'placeholder' => __( 'Enter your last name', 'ninja' ),
+						'order'       => 10,
+					],
+					'phone_number'       => [ 
+						'class'       => 'form-field col-6 pr-3',
+						'type'        => 'text',
+						'label'       => __( 'Phone Number', 'ninja' ),
+						'name'        => 'phone_number',
+						'required'    => TRUE,
+						'placeholder' => __( 'Enter your phone number', 'ninja' ),
+						'order'       => 15,
+					],
+					'user_email'         => [ 
+						'class'       => 'form-field col-6 pl-3',
+						'type'        => 'email',
+						'label'       => __( 'Email', 'ninja' ),
+						'name'        => 'user_email',
+						'required'    => TRUE,
+						'placeholder' => __( 'Enter your email', 'ninja' ),
+						'order'       => 20,
+					],
+					'user_password'      => [ 
+						'class'       => 'form-field col-6 pr-3',
+						'type'        => 'password',
+						'label'       => __( 'Password', 'ninja' ),
+						'name'        => 'user_password',
+						'required'    => TRUE,
+						'placeholder' => __( 'Enter your password', 'ninja' ),
+						'before'      => '<i class="fa-sharp fa-solid fa-eye-slash showPassIcon" data-target ="#' . Nh::_DOMAIN_NAME . '_user_password"></i>',
+						'order'       => 25,
+					],
+					'confirm_password'   => [ 
+						'class'       => 'form-field col-6 pl-3',
+						'type'        => 'password',
+						'label'       => __( 'Confirm Password', 'ninja' ),
+						'name'        => 'confirm_password',
+						'required'    => TRUE,
+						'placeholder' => __( 'Enter your confirm password', 'ninja' ),
+						'before'      => '<i class="fa-sharp fa-solid fa-eye-slash showPassIcon" data-target ="#' . Nh::_DOMAIN_NAME . '_confirm_password"></i>',
+						'order'       => 30,
+					],
+					'user_type'          => [ 
+						'type'           => 'select',
+						'label'          => __( 'User Type', 'ninja' ),
+						'name'           => 'user_type',
+						'required'       => TRUE,
+						'placeholder'    => __( 'Enter your user type', 'ninja' ),
+						'options'        => [ 
+							'owner'    => __( 'I am Owner', 'ninja' ),
+							'investor' => __( 'I am Investor', 'ninja' ),
+						],
+						'default_option' => '',
+						'select_option'  => [ $user_type ],
+						'class'          => 'form-field col-6 pr-3',
+						'order'          => 35,
+					],
+					'verification_type'  => [ 
+						'type'           => 'select',
+						'label'          => __( 'Account Verification Type', 'ninja' ),
+						'name'           => 'verification_type',
+						'required'       => TRUE,
+						'placeholder'    => __( 'Enter your verification type', 'ninja' ),
+						'options'        => [ 
+							Nh_User::VERIFICATION_TYPES['email'] => __( 'Email', 'ninja' ),
+							Nh_User::VERIFICATION_TYPES['mobile'] => __( 'Phone Number', 'ninja' ),
+							Nh_User::VERIFICATION_TYPES['whatsapp'] => __( 'Whatsapp', 'ninja' ),
+						],
+						'default_option' => '',
+						'select_option'  => '',
+						'class'          => 'form-field col-6 pl-3',
+						'order'          => 40,
+					],
+					'custom-html-3'      => [ 
+						'type'    => 'html',
+						'content' => '</div>',
+						'order'   => 45,
+					],
+					'registration_nonce' => [ 
+						'class' => '',
+						'type'  => 'nonce',
+						'name'  => 'registration_nonce',
+						'value' => Nh::_DOMAIN_NAME . "_registration_form",
+						'order' => 50
+					],
+					'submit'             => [ 
+						'class'               => 'form-action bbc-btn btn-primary large apply',
+						'type'                => 'submit',
+						'value'               => __( 'Create Account', 'ninja' ),
+						'before'              => '',
+						'after'               => '',
+						'recaptcha_form_name' => 'frontend_registration',
+						'order'               => 55
+					],
+				], [ 
+					'class' => Nh::_DOMAIN_NAME . '-registration-form',
+					'id'    => Nh::_DOMAIN_NAME . '_registration_form'
+				] );
+			/** TODO!: create function to return copyrights */
+			?>
+			<div class="section-footer">
+				<p>Copyright © 2023 B2B All rights reserved.</p>
+			</div>
 		</section>
-		<section class="login-animation h-100 col-12 col-md-6">
+		<section class="login-animation col-12 col-md-6">
 			<div id="loginCarousel" class="carousel slide w-100 h-100">
 				<div class="carousel-navigation">
 					<button class="carousel-control-prev" type="button" data-bs-target="#loginCarousel" data-bs-slide="prev">
