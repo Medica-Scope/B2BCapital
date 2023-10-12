b92332ebfab4fc7400f27ac7de13c79bba7880fc
Khaled Fathy <kfathy194@gmail.com>
2023-10-12 11:23:14 +0300

    implement dashboard ui

---
+++ wp-content/themes/ninjahub-pro/app/Views/template-parts/opportunity-card-vertical.php
@@ -0,0 +1,63 @@
+<?php
+$opportunity_link         = ! empty( $args['opportunity_link'] ) ? $args['opportunity_link'] : false;
+$opportunity_title        = ! empty( $args['opportunity_title'] ) ? $args['opportunity_title'] : false;
+$opportunity_thumbnail    = ! empty( $args['opportunity_thumbnail'] ) ? $args['opportunity_thumbnail'] : false;
+$opportunity_created_date = ! empty( $args['opportunity_created_date'] ) ? $args['opportunity_created_date'] : false;
+$is_item_controllers      = ! empty( $args['is_item_controllers'] ) ? $args['is_item_controllers'] : false;
+$is_card_horizontal       = ! empty( $args['is_card_horizontal'] ) ? $args['is_card_horizontal'] : false;
+?>
+<div class="opportunity-item card shadow border-0">
+	<div class="row g-0">
+		<div class="card-img-top <?php echo $is_card_horizontal ? 'col-md-4' : ''; ?>">
+			<a href="<?= esc_url( $opportunity_link ); ?>"><img src="<?= esc_url( $opportunity_thumbnail ); ?>"
+					alt="<?= esc_attr( $opportunity_title ); ?>"></a>
+			<?php
+			if ( $is_item_controllers ) {
+				?>
+				<div class="opportunity-item-controllers">
+					<button class="btn btn-light bg-white opportunity-to-favorite">
+						<i class="bbc-star-o"></i>
+					</button>
+					<button class="btn btn-light bg-white opportunity-btn-more">
+						<i class="bbc-more-vertical"></i>
+					</button>
+				</div>
+				<?php
+			}
+			?>
+		</div>
+		<div class="card-body p-0 <?php echo $is_card_horizontal ? 'col-md-8' : ''; ?>">
+			<p class="card-text"><small class="text-body-secondary">
+					<?= __( 'Business Type', 'ninja' ); ?>
+				</small></p>
+
+			<a href="<?= esc_url( $opportunity_link ); ?>" class="card-title btn btn-link btn-link-dark">
+				<?= $opportunity_title; ?>
+			</a>
+
+			<p class="card-text"><small class="text-body-secondary">
+					<?= $opportunity_created_date; ?>
+				</small>
+			</p>
+
+			<div class="card-extra-info">
+				<div class="card-info-item">
+					<small class="text-body-secondary">Location</small>
+					<p class="card-text fw-bold">Egypt</p>
+				</div>
+				<div class="card-info-item">
+					<small class="text-body-secondary">Valuation</small>
+					<p class="card-text fw-bold">95460$</p>
+				</div>
+				<div class="card-info-item">
+					<small class="text-body-secondary">Location</small>
+					<p class="card-text fw-bold">Egypt</p>
+				</div>
+				<div class="card-info-item">
+					<small class="text-body-secondary">Valuation</small>
+					<p class="card-text fw-bold">95460$</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- wp-content/themes/ninjahub-pro/templates/template-page-dashboard.php
+++ wp-content/themes/ninjahub-pro/templates/template-page-dashboard.php
@@ -1,116 +1,174 @@
 <?php
-    /**
-     * @Filename: template-page-dashboard.php
-     * @Description:
-     * @User: NINJA MASTER - Mustafa Shaaban
-     * @Date: 21/2/2023
-     *
-     * Template Name: Dashboard Page
-     * Template Post Type: page
-     *
-     * @package NinjaHub
-     * @since 1.0
-     *
-     */
+/**
+ * @Filename: template-page-dashboard.php
+ * @Description:
+ * @User: NINJA MASTER - Mustafa Shaaban
+ * @Date: 21/2/2023
+ *
+ * Template Name: Dashboard Page
+ * Template Post Type: page
+ *
+ * @package NinjaHub
+ * @since 1.0
+ *
+ */
 
 
-    use NH\APP\MODELS\FRONT\MODULES\Nh_Notification;
-    use NH\APP\MODELS\FRONT\MODULES\Nh_Opportunity;
-    use NH\APP\MODELS\FRONT\Nh_Public;
+use NH\APP\MODELS\FRONT\MODULES\Nh_Notification;
+use NH\APP\MODELS\FRONT\MODULES\Nh_Opportunity;
+use NH\APP\MODELS\FRONT\Nh_Public;
+use NH\APP\HELPERS\Nh_Hooks;
+use NH\Nh;
 
-    global $user_ID;
+global $user_ID;
 
-    get_header();
+get_header();
+
+Nh_Hooks::enqueue_style( Nh::_DOMAIN_NAME . '-public-style-home-dashboard', Nh_Hooks::PATHS['public']['css'] . '/pages/dashboard/home-dashboard' );
 ?>
 
-    <main id="" class="site-dashboard-home">
-        <?php Nh_Public::breadcrumbs(); ?>
+<main class="site-dashboard-home">
+	<div class="container">
+		<div class="dashboard-overview">
+			<h3 class="fs-3 text-primary">
+				<?= __( 'Overview', 'ninja' ) ?>
+			</h3>
+			<div class="widget-list">
+				<div class="single-widget">
+					widget 1
+				</div>
+			</div>
+		</div>
 
-        <div class="container-fluid">
-            <div class="nh-dashboard-overview">
-                <h3><?= __('Overview', 'ninja') ?></h3>
-                <div class="widget-list">
-                    <div class="single-widget">
-                        widget 1
-                    </div>
-                </div>
-            </div>
 
-            <hr>
+		<section class="dashboard-body">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="dashboard-opportunity-list-container">
+						<div class="opportunity-list-header row justify-content-between align-items-center">
+							<div class="opportunity-title col-6">
+								<h3 class="fs-3 text-primary">
+									<?= __( 'Latest Opportunities', 'ninja' ) ?>
+								</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores eaque eos
+									fuga neque,
+									pariatur.</p>
+							</div>
+							<div class="opportunity-actions col-6">
+								<div class="opportunity-list-sort">
+									<button class="btn btn-light bg-transparent opportunity-list-style">
+										<i class="bbc-th-list"></i>
+									</button>
+									<button class="btn btn-light bg-transparent opportunity-list-filter">
+										<i class="bbc-grid"></i>
+									</button>
+								</div>
+								<button class="btn btn-outline-warning opportunity-adv-filter"
+									data-bs-toggle="button"><i class="bbc-sliders"></i> Advanced Filters</button>
+							</div>
+						</div>
+						<div class="opportunity-list">
+							<div class="row row-cols-1 row-cols-md-2 g-4 card-group">
+								<?php
+								$opportunities_obj = new Nh_Opportunity();
+								$opportunities     = $opportunities_obj->get_all();
 
-            <section class="dashboard-body">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="dashboard-opportunity-list-container">
-                            <header class="opportunity-list-header d-flex justify-content-between align-items-center">
-                                <div class="opportunity-title">
-                                    <h3><?= __('Latest Opportunities', 'ninja') ?></h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores eaque eos fuga neque, pariatur.</p>
-                                </div>
-                                <div class="opportunity-filters d-flex justify-content-center">
-                                    <div class="opportunity-list-style">Style</div>
-                                    <div class="opportunity-list-filter">Filter</div>
-                                </div>
-                            </header>
-                            <div class="opportunity-list">
-                                <div class="row">
-                                    <?php
+								foreach ( $opportunities as $opportunity ) {
+									$args = [ 
+										'opportunity_link'         => $opportunity->link,
+										'opportunity_title'        => $opportunity->title,
+										'opportunity_thumbnail'    => $opportunity->thumbnail,
+										'opportunity_created_date' => $opportunity->created_date,
+										'is_item_controllers'      => true,
+									];
+									?>
+									<div class="col">
+										<?php get_template_part( 'app/Views/template-parts/opportunity-card-vertical', null,
+											$args ); ?>
+									</div>
+									<?php
+								}
+								?>
+							</div>
+						</div>
+					</div>
+				</div>
 
-                                        $opportunities_obj = new Nh_Opportunity();
-                                        $opportunities     = $opportunities_obj->get_all();
+				<div class="col-md-4">
+					<div class="sidebar-container">
+						<div class="sidebar-create-opportunity shadow">
+							<a href="<?= apply_filters( 'nhml_permalink', get_permalink( get_page_by_path( 'dashboard/create-opportunity' ) ) ); ?>"
+								class="btn-create-opportunity btn btn-success btn-lg shadow text-uppercase">
+								<i class="bbc-plus-square"></i>
+								<?= __( 'Create New Opportunity', 'ninja' ) ?>
+							</a>
 
-                                        foreach ($opportunities as $opportunity) {
-                                            ?>
-                                            <div class="col-md-6">
-                                                <div class="opportunity-item">
-                                                    <div class="thumbnail">
-                                                        <a href="<?= $opportunity->link ?>"><img src="<?= $opportunity->thumbnail ?>" alt="<?= $opportunity->title ?>"></a>
-                                                        <div class="item-controllers">
-                                                            <button class="btn opportunity-to-favorite">Fav Icon</button>
-                                                            <button class="btn">:</button>
-                                                        </div>
-                                                    </div>
-                                                    <div class="opportunity-content">
-                                                        <a href="<?= $opportunity->link ?>">
-                                                            <h5><?= $opportunity->title ?></h5>
-                                                            <h6><?= __('Business Type', 'ninja') ?></h6>
-                                                            <p>test, test2</p>
-                                                            <h6><?= $opportunity->created_date ?></h6>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <?php
-                                        }
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="side-bar-container">
-                            <div class="sidebar-create-opportunity">
-                                <a href="<?= apply_filters('nhml_permalink', get_permalink(get_page_by_path('dashboard/create-opportunity'))) ?>"><?= __('Create New Opportunity', 'ninja') ?></a>
-                                <span><?= __('Please Note You Can Add Only One Opportunity In Month', 'ninja') ?></span>
-                            </div>
-                            <div class="sidebar-my-opportunities">
-                                <h3><?= __('My Opportunities', 'ninja') ?></h3>
-                            </div>
-                            <div class="sidebar-latest-acquisitions">
-                                <h3><?= __('Latest acquisitions', 'ninja') ?></h3>
-                            </div>
-                            <div class="sidebar-faq">
-                                <h3><?= __('Help and Modules', 'ninja') ?></h3>
-                                <p><?= __('Our help and module screens are here to make your life easier', 'ninja') ?></p>
-                                <a href="<?= apply_filters( 'nhml_permalink', get_post_type_archive_link('faq') ) ?>"><?= __('Go', 'ninja') ?></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+							<small class="pt-3"><i class="bbc-lightbulb-o"></i>
+								<?= __( 'Please Note You Can Add Only', 'ninja' ); ?> <span class="text-warning">
+									<?= __( 'One Opportunity In Month', 'ninja' ) ?>
+								</span>
+							</small>
 
-    </main><!-- #main -->
+						</div>
+						<div class="sidebar-my-opportunities shadow">
+							<h3 class="text-primary">
+								<?= __( 'My Opportunities', 'ninja' ) ?>
+							</h3>
+						</div>
+						<div class="sidebar-latest-acquisitions shadow">
+							<div>
+								<h3 class="text-primary">
+									<?= __( 'Latest acquisitions', 'ninja' ) ?>
+								</h3>
+								<small class="text-muted">Latest update in June 28th, 2023</small>
+							</div>
+							<div class="acquisitions-list">
+
+								<div class="row row-cols-1 g-4 card-group">
+									<?php
+									$opportunities_obj = new Nh_Opportunity();
+									$opportunities     = $opportunities_obj->get_all();
+
+									foreach ( $opportunities as $opportunity ) {
+										$args = [ 
+											'opportunity_link'         => $opportunity->link,
+											'opportunity_title'        => $opportunity->title,
+											'opportunity_thumbnail'    => $opportunity->thumbnail,
+											'opportunity_created_date' => $opportunity->created_date,
+											'is_item_controllers'      => false,
+										];
+										?>
+										<div class="col">
+											<?php get_template_part( 'app/Views/template-parts/opportunity-card-horizontal', null,
+												$args ); ?>
+										</div>
+										<?php
+									}
+									?>
+								</div>
+							</div>
+							<div class="sidebar-faq shadow">
+								<div>
+									<h3>
+										<?= __( 'Help and Modules', 'ninja' ) ?>
+									</h3>
+									<p>
+										<?= __( 'Our help and module screens are here to make your life easier', 'ninja' ) ?>
+									</p>
+									<a href="<?= apply_filters( 'nhml_permalink', get_post_type_archive_link( 'faq' ) ) ?>"
+										class="btn square">
+										<?= __( 'Go', 'ninja' ) ?>
+									</a>
+								</div>
+
+								<img src="<?php echo Nh_Hooks::PATHS['public']['img']; ?>/help.webp" alt="help icon" />
+							</div>
+						</div>
+					</div>
+				</div>
+		</section>
+	</div>
+
+</main><!-- #main -->
 
 <?php get_footer();
-
