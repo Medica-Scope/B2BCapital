e35a110d9732e1aae10b0b065f034ed7d3fa631a
Ahmed Gamal <ahmed.gamal@mitchdesgins.com>
2023-10-12 17:28:07 +0300

    feature/fav-ignore-opportunity

--- wp-content/themes/ninjahub-pro/app/Models/public/assets/js/modules/Opportunity.js
+++ wp-content/themes/ninjahub-pro/app/Models/public/assets/js/modules/Opportunity.js
@@ -269,6 +269,79 @@ class NhOpportunity extends Nh
             },
         });
     }
+
+    toggleFavoriteOpportunity($el,user_id,post_id)
+    {
+        let that                      = this;
+        this.ajaxRequests.toggleFav = $.ajax({
+            url: nhGlobals.ajaxUrl,
+            type: 'POST',
+            data: {
+                action: `${KEY}_toggle_favorite_opportunity_ajax`,
+                user_id: user_id,
+                post_id: post_id,
+            },
+            beforeSend: function () {
+                UiCtrl.beforeSendPrepare($el);
+            },
+            success: function (res) {
+                if (res.success) {
+                    if(res.data.fav_active == 0){
+                        $el.addClass('btn-dark');
+                    }else{
+                        $el.removeClass('btn-dark');
+                    }
+                    UiCtrl.blockUI($el, false);
+
+                }
+            },
+            error: function (xhr) {
+                let errorMessage = `${xhr.status}: ${xhr.statusText}`;
+                if (xhr.statusText !== 'abort') {
+                    console.error(errorMessage);
+                }
+            }
+        });
+    }
+
+    ignoreOpportunity($el,user_id,post_id)
+    {
+        let that                      = this;
+        this.ajaxRequests.toggleFav = $.ajax({
+            url: nhGlobals.ajaxUrl,
+            type: 'POST',
+            data: {
+                action: `${KEY}_ignore_opportunity_ajax`,
+                user_id: user_id,
+                post_id: post_id,
+            },
+            beforeSend: function () {
+                UiCtrl.beforeSendPrepare($el);
+            },
+            success: function (res) {
+                if (res.success) {
+                    if(res.data.ignore_active == 0){
+                        $el.removeClass('btn-outline-light');
+                        $el.addClass('btn-outline-dark');
+                    }else{
+                        $el.removeClass('btn-outline-dark');
+                        $el.addClass('btn-outline-light');
+                    }
+                    if(res.data.updated){
+                        $('.opportunity-content').html(res.data.updated);
+                    }
+                    UiCtrl.blockUI($el, false);
+
+                }
+            },
+            error: function (xhr) {
+                let errorMessage = `${xhr.status}: ${xhr.statusText}`;
+                if (xhr.statusText !== 'abort') {
+                    console.error(errorMessage);
+                }
+            }
+        });
+    }
 }
 
 export default NhOpportunity;
