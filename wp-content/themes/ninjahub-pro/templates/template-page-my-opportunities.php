ad2728c1d40d31a65f98b51716335466c901a123
Khaled Fathy <kfathy194@gmail.com>
2023-10-17 23:03:33 +0300

    fix about-us styling issues

--- wp-content/themes/ninjahub-pro/templates/template-page-my-opportunities.php
+++ wp-content/themes/ninjahub-pro/templates/template-page-my-opportunities.php
@@ -1,106 +1,112 @@
 <?php
 
-    /**
-     * @Filename: template-my-opportunities.php
-     * @Description:
-     * @User: NINJA MASTER - Mustafa Shaaban
-     * @Date: 21/2/2023
-     *
-     * Template Name: My Opportunities Page
-     * Template Post Type: page
-     *
-     * @package NinjaHub
-     * @since 1.0
-     */
+/**
+ * @Filename: template-my-opportunities.php
+ * @Description:
+ * @User: NINJA MASTER - Mustafa Shaaban
+ * @Date: 21/2/2023
+ *
+ * Template Name: My Opportunities Page
+ * Template Post Type: page
+ *
+ * @package NinjaHub
+ * @since 1.0
+ */
 
 
-    use NH\APP\CLASSES\Nh_User;
-    use NH\APP\HELPERS\Nh_Hooks;
-    use NH\APP\MODELS\FRONT\MODULES\Nh_Opportunity;
-    use NH\APP\MODELS\FRONT\MODULES\Nh_Opportunity_Acquisition;
-    use NH\APP\MODELS\FRONT\MODULES\Nh_Profile;
-    use NH\APP\MODELS\FRONT\Nh_Public;
-    use NH\Nh;
+use NH\APP\CLASSES\Nh_User;
+use NH\APP\HELPERS\Nh_Hooks;
+use NH\APP\MODELS\FRONT\MODULES\Nh_Opportunity;
+use NH\APP\MODELS\FRONT\MODULES\Nh_Opportunity_Acquisition;
+use NH\APP\MODELS\FRONT\MODULES\Nh_Profile;
+use NH\APP\MODELS\FRONT\Nh_Public;
+use NH\Nh;
 
-    get_header();
+get_header();
 
-    Nh_Hooks::enqueue_style(Nh::_DOMAIN_NAME . '-public-style-my-account', Nh_Hooks::PATHS['public']['css'] . '/pages/dashboard/my-account');
-    Nh_Hooks::enqueue_style(Nh::_DOMAIN_NAME . '-public-style-my-opportunities', Nh_Hooks::PATHS['public']['css'] . '/pages/dashboard/my-opportunities');
+Nh_Hooks::enqueue_style( Nh::_DOMAIN_NAME . '-public-style-my-account', Nh_Hooks::PATHS['public']['css'] . '/pages/dashboard/my-account' );
+Nh_Hooks::enqueue_style( Nh::_DOMAIN_NAME . '-public-style-my-opportunities', Nh_Hooks::PATHS['public']['css'] . '/pages/dashboard/my-opportunities' );
 
-    global $user_ID;
-    $opportunity_obj  = new Nh_Opportunity();
-    $acquisitions_obj = new Nh_Opportunity_Acquisition();
-    $opportunities    = $opportunity_obj->get_profile_opportunities();
-    $acquisitions     = $acquisitions_obj->get_profile_acquisitions();
+global $user_ID;
+$opportunity_obj  = new Nh_Opportunity();
+$acquisitions_obj = new Nh_Opportunity_Acquisition();
+$opportunities    = $opportunity_obj->get_profile_opportunities();
+$acquisitions     = $acquisitions_obj->get_profile_acquisitions();
 
 ?>
-    <main class="my-opportunities">
-        <div class="container container-xxl">
-            <?php Nh_Public::breadcrumbs(); ?>
+<main class="my-opportunities">
+	<div class="container container-xxl">
+		<?php Nh_Public::breadcrumbs(); ?>
 
-            <nav class="dashboard-submenus mt-3 mb-5">
-                <?php get_template_part('app/Views/template-parts/dashboard-submenus/main-nav', NULL, [ 'active_link' => 'opportunities' ]); ?>
-                <?php get_template_part('app/Views/template-parts/dashboard-submenus/opportunities-sub-nav', NULL, [ 'active_link' => 'opportunities' ]); ?>
-            </nav>
+		<nav class="dashboard-submenus mt-3 mb-5">
+			<?php get_template_part( 'app/Views/template-parts/dashboard-submenus/main-nav', NULL, [ 'active_link' => 'opportunities' ] ); ?>
+			<?php get_template_part( 'app/Views/template-parts/dashboard-submenus/opportunities-sub-nav', NULL, [ 'active_link' => 'opportunities' ] ); ?>
+		</nav>
 
-            <section class="my-opportunities container">
-                <?php
+		<section class="my-opportunities container">
+			<?php
 
-                    if (Nh_User::get_user_role() === Nh_User::INVESTOR) {
-                        ?>
-                        <div class="row row-cols-1 row-cols-md-3 g-4">
-                            <?php
-                                foreach ($acquisitions as $acquisition) {
-                                    ?>
-                                    <div class="opportunity-card">
+			if ( Nh_User::get_user_role() === Nh_User::INVESTOR ) {
+				?>
+				<div class="row row-cols-1 row-cols-md-3 g-4">
+					<?php
+					foreach ( $acquisitions as $acquisition ) {
+						?>
+						<div class="opportunity-card">
 
-                                        <h3>
-                                            <a href="<?= $acquisition->opportunity->link ?>"><?php echo $acquisition->opportunity->title; ?></a>
-                                        </h3>
+							<h3>
+								<a href="<?= $acquisition->opportunity->link ?>">
+									<?php echo $acquisition->opportunity->title; ?>
+								</a>
+							</h3>
 
-                                        <span class="date"><?php echo date('F jS, Y', strtotime($acquisition->opportunity->created_date)); ?></span>
+							<span class="date">
+								<?php echo date( 'F jS, Y', strtotime( $acquisition->opportunity->created_date ) ); ?>
+							</span>
 
-                                        <p class="short-description">
-                                            <?php echo $acquisition->opportunity->meta_data['short_description']; ?>
-                                        </p>
+							<p class="short-description">
+								<?php echo $acquisition->opportunity->meta_data['short_description']; ?>
+							</p>
 
-                                        <span class="status"><?php echo $acquisition->meta_data['acquisition_stage']; ?></span>
+							<span class="status">
+								<?php echo $acquisition->meta_data['acquisition_stage']; ?>
+							</span>
 
-                                    </div>
-                                    <?php
-                                }
-                            ?>
-                        </div> <!-- </row-cols-1 -->
-                        <?php
-                    } else {
-                        ?>
+						</div>
+						<?php
+					}
+					?>
+				</div> <!-- </row-cols-1 -->
+				<?php
+			} else {
+				?>
 
-                        <div class="row row-cols-1 row-cols-md-2 g-4">
-                            <?php
-                                foreach ($opportunities as $opportunity) {
-                                    ?>
-                                    <div class="col">
-                                        <?php
-                                            get_template_part('app/Views/template-parts/cards/my-opportunities-card', NULL, [
-                                                    'opportunity_title'             => $opportunity->title,
-                                                    'opportunity_link'             => $opportunity->link,
-                                                    'opportunity_modified'          => $opportunity->modified,
-                                                    'opportunity_created_date'      => $opportunity->created_date,
-                                                    'opportunity_short_description' => $opportunity->meta_data['short_description'],
-                                                    'opportunity_stage'             => $opportunity->meta_data['opportunity_stage'],
-                                                ]);
-                                        ?>
-                                    </div>
-                                    <?php
-                                }
-                            ?>
-                        </div> <!-- </row-cols-1 -->
-                        <?php
-                    }
-                ?>
-            </section>
-        </div>
-    </main><!-- #main -->
+				<div class="row row-cols-1 row-cols-md-2 g-4">
+					<?php
+					foreach ( $opportunities as $opportunity ) {
+						?>
+						<div class="col">
+							<?php
+							get_template_part( 'app/Views/template-parts/cards/my-opportunities-card', NULL, [ 
+								'opportunity_title'             => $opportunity->title,
+								'opportunity_link'              => $opportunity->link,
+								'opportunity_modified'          => $opportunity->modified,
+								'opportunity_created_date'      => $opportunity->created_date,
+								'opportunity_short_description' => $opportunity->meta_data['short_description'],
+								'opportunity_stage'             => $opportunity->meta_data['opportunity_stage'],
+							] );
+							?>
+						</div>
+						<?php
+					}
+					?>
+				</div> <!-- </row-cols-1 -->
+				<?php
+			}
+			?>
+		</section>
+	</div>
+</main><!-- #main -->
 
 <?php
-    get_footer();
+get_footer();
