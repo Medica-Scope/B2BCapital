26c8d5cd2739fa4660639cc9df51b1acc445c9b8
Ninja Master <mustafa.shaaban@appenza-studio.com>
2023-10-17 16:00:48 +0300

    Second Page Account

--- wp-content/themes/ninjahub-pro/app/Views/js-templates/modals/auth-verif.php
+++ wp-content/themes/ninjahub-pro/app/Views/js-templates/modals/auth-verif.php
@@ -14,7 +14,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="authVerifySuccessLabel"><%= msg %></h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <p><%= msg %></p>
