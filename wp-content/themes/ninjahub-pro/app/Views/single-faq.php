9679af51151db61fe06454e865d28960ae8741c9
Khaled Fathy <kfathy194@gmail.com>
2023-10-26 18:19:23 +0300

    implement faq-taxonomy template ui

--- wp-content/themes/ninjahub-pro/app/Views/single-faq.php
+++ wp-content/themes/ninjahub-pro/app/Views/single-faq.php
@@ -8,30 +8,30 @@
  */
 
 use NH\APP\HELPERS\Nh_Hooks;
+use NH\APP\MODELS\FRONT\Nh_Public;
 use NH\APP\MODELS\FRONT\MODULES\Nh_Faq;
 use NH\Nh;
 
 get_header();
 
-Nh_Hooks::enqueue_style(Nh::_DOMAIN_NAME . '-public-style-home-landing', Nh_Hooks::PATHS['public']['css'] . '/pages/landing/home');
+
 global $post;
 $single = new Nh_Faq();
-$single = $single->convert($post);
+$single = $single->convert( $post );
 ?>
-<div class="accordion accordion-flush" id="accordionFlush">
-    <div class="accordion-item" data-id="<?= $single->ID ?>">
+<section class="page-content">
 
-        <div class="accordion-header" id="flush-heading<?= $single->ID ?>">
-            <h3 class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush<?= $single->ID ?>" aria-expanded="false" aria-controls="flush<?= $single->ID ?>">
-                <?= $single->title ?>
-            </h3>
-        </div>
-        <div id="flush<?= $single->ID ?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?= $single->ID ?>" data-bs-parent="#accordionFlush">
-            <div class="accordion-body"><?= $single->content ?></div>
-        </div>
-    </div>
-</div>
+	<?php Nh_Public::breadcrumbs(); ?>
 
+	<h1 class="page-title mb-4">
+		<?= $single->title ?>
+	</h1>
+	<div class="">
+		<?= $single->content ?>
+	</div>
+
+
+</section>
 </div> <!-- </layout> -->
 </main>
 </div> <!-- </landing-page> -->
