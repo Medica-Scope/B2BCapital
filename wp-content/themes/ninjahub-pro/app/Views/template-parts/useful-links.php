83d611f7e2f678f874f8d10ca091215049ca0a4c
Khaled Fathy <kfathy194@gmail.com>
2023-10-26 16:13:15 +0300

    implement archive-faq ui template

--- wp-content/themes/ninjahub-pro/app/Views/template-parts/useful-links.php
+++ wp-content/themes/ninjahub-pro/app/Views/template-parts/useful-links.php
@@ -11,13 +11,25 @@ use NH\APP\HELPERS\Nh_Hooks;
 ?>
 
 <div class="useful-links-con">
-    <img src="<?= Nh_Hooks::PATHS['public']['img'] ?>/links.webp" alt="B2B" />
-    <h3><?= __("Useful links for you", "ninja") ?></h3>
-    <ul class="links">
-        <li><a href="#"><?= __("Order return form", "ninja") ?></a></li>
-        <li><a href="#"><?= __("Contact form", "ninja") ?></a></li>
-        <li><a href="#"><?= __("Shipping pricing table", "ninja") ?></a></li>
-        <li><a href="#"><?= __("Reviews", "ninja") ?></a></li>
-        <li><a href="#"><?= __("Your orders", "ninja") ?></a></li>
-    </ul>
+	<h3>
+		<i class="bbc-link2"></i>
+		<?= __( "Useful links for you", "ninja" ) ?>
+	</h3>
+	<ul class="links">
+		<li><a href="#" class="title btn btn-link text-dark">
+				<?= __( "Order return form", "ninja" ) ?>
+			</a></li>
+		<li><a href="#" class="title btn btn-link text-dark">
+				<?= __( "Contact form", "ninja" ) ?>
+			</a></li>
+		<li><a href="#" class="title btn btn-link text-dark">
+				<?= __( "Shipping pricing table", "ninja" ) ?>
+			</a></li>
+		<li><a href="#" class="title btn btn-link text-dark">
+				<?= __( "Reviews", "ninja" ) ?>
+			</a></li>
+		<li><a href="#" class="title btn btn-link text-dark">
+				<?= __( "Your orders", "ninja" ) ?>
+			</a></li>
+	</ul>
 </div>
--- wp-content/themes/ninjahub-pro/archive.php
+++ wp-content/themes/ninjahub-pro/archive.php
@@ -1,73 +1,74 @@
 <?php
 
-    /**
-     * The template for displaying archive pages
-     *
-     * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
-     *
-     * @package NinjaHub
-     */
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package NinjaHub
+ */
 
-    use NH\APP\HELPERS\Nh_Forms;
-    use NH\APP\HELPERS\Nh_Hooks;
-    use NH\APP\MODELS\FRONT\MODULES\Nh_Faq;
-    use NH\APP\MODELS\FRONT\Nh_Public;
-    use NH\Nh;
+use NH\APP\HELPERS\Nh_Forms;
+use NH\APP\HELPERS\Nh_Hooks;
+use NH\APP\MODELS\FRONT\MODULES\Nh_Faq;
+use NH\APP\MODELS\FRONT\Nh_Public;
+use NH\Nh;
 
-    get_header();
-    Nh_Hooks::enqueue_style(Nh::_DOMAIN_NAME . '-public-style-home-landing', Nh_Hooks::PATHS['public']['css'] . '/pages/landing/home');
+get_header();
 
-    global $wp_query, $post;
-    $queried_post_type = $wp_query->query;
+Nh_Hooks::enqueue_style( Nh::_DOMAIN_NAME . '-public-style-home-dashboard', Nh_Hooks::PATHS['public']['css'] . '/pages/dashboard/home-dashboard' );
+
+global $wp_query, $post;
+$queried_post_type = $wp_query->query;
 ?>
 
-    <main id="" class="">
-        <?php
-            if (is_tax()) {
-                $term_object = $wp_query->get_queried_object();
-                if (empty(locate_template('app/Views/single-taxonomy-' . $term_object->taxonomy . '.php'))) {
-                    get_template_part('app/Views/single-taxonomy');
-                } else {
-                    get_template_part('app/Views/single-taxonomy', $term_object->taxonomy);
-                }
-            } else {
+<main id="" class="container container-xxl">
+	<?php
+	if ( is_tax() ) {
+		$term_object = $wp_query->get_queried_object();
+		if ( empty( locate_template( 'app/Views/single-taxonomy-' . $term_object->taxonomy . '.php' ) ) ) {
+			get_template_part( 'app/Views/single-taxonomy' );
+		} else {
+			get_template_part( 'app/Views/single-taxonomy', $term_object->taxonomy );
+		}
+	} else {
 
-                if ($queried_post_type['post_type'] !== 'service') {
-                    Nh_Public::breadcrumbs();
-                }
+		if ( $queried_post_type['post_type'] !== 'service' ) {
+			Nh_Public::breadcrumbs();
+		}
 
-                if (empty(locate_template('app/Views/archive-' . get_post_type() . '.php'))) {
-                    get_template_part('app/Views/archive');
-                } else {
-                    get_template_part('app/Views/archive', get_post_type());
-                }
-            }
+		if ( empty( locate_template( 'app/Views/archive-' . get_post_type() . '.php' ) ) ) {
+			get_template_part( 'app/Views/archive' );
+		} else {
+			get_template_part( 'app/Views/archive', get_post_type() );
+		}
+	}
 
-        ?>
-        <?php
-            //         if (have_posts() && $queried_post_type['post_type'] !== 'faq') :
-
-            /* Start the Loop */
-            //            while (have_posts()) :
-            //                the_post();
-            //
-            //                /*
-            //                     * Include the Post-Type-specific template for the content.
-            //                     * If you want to override this in a child theme, then include a file
-            //                     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-            //                     */
+	?>
+	<?php
+	//         if (have_posts() && $queried_post_type['post_type'] !== 'faq') :
+	
+	/* Start the Loop */
+	//            while (have_posts()) :
+	//                the_post();
+	//
+	//                /*
+	//                     * Include the Post-Type-specific template for the content.
+	//                     * If you want to override this in a child theme, then include a file
+	//                     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+	//                     */
 //                            if (empty(locate_template('app/Views/archive-' . get_post_type() . '.php'))) {
 //                                get_template_part('app/Views/archive');
 //                            } else {
 //                                get_template_part('app/Views/archive', get_post_type());
 //                            }
-            //
-            //            endwhile;
-            //        endif;
-        ?>
+	//
+	//            endwhile;
+	//        endif;
+	?>
 
-    </main><!-- #main -->
+</main><!-- #main -->
 
 <?php
-    // get_sidebar();
-    get_footer();
+// get_sidebar();
+get_footer();
