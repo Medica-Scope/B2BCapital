8fc8ad94de2590ce80a94137fe88c643ff1a6a98
Ninja Master <Mustafashaaban22@gmail.com>
2024-04-30 00:48:04 +0300

    debug

--- wp-content/themes/ninjahub-pro/templates/template-page-industry.php
+++ wp-content/themes/ninjahub-pro/templates/template-page-industry.php
@@ -240,7 +240,7 @@ $opportunity_obj = new Nh_Opportunity();
 
 
 				'size_of_investment'                   => [
-					'class'   => 'container dasdasdasd' . Nh_User::get_user_role() !== Nh_User::INVESTOR ? 'd-none' : '' . Nh_User::get_user_role(),
+					'class'   => 'container ',
 					'type'    => 'radio',
 					'name'    => 'size_of_investment',
 					'title'   => __( '- What is the size of your investment ?', 'ninja' ),
@@ -280,7 +280,7 @@ $opportunity_obj = new Nh_Opportunity();
 				],
 
 				'investment_criteria'                => [
-					'class'       => Nh_User::get_user_role() !== Nh_User::INVESTOR ? 'd-none' : '',
+					'class'       => '',
 					'type'        => 'text',
 					'label'       => __('- What is Your Investment criteria?', 'ninja'),
 					'name'        => 'investment_criteria',
@@ -290,7 +290,7 @@ $opportunity_obj = new Nh_Opportunity();
 					'order'       => 35,
 				],
 				'external_or_long_term'                => [
-					'class'   => 'container ' . Nh_User::get_user_role() !== Nh_User::INVESTOR ? 'd-none' : '',
+					'class'   => '',
 					'type'    => 'radio',
 					'name'    => 'external_or_long_term',
 					'title'   => __( '- External or long-term?', 'ninja' ),
@@ -319,7 +319,7 @@ $opportunity_obj = new Nh_Opportunity();
 					'order'       => 35,
 				],
 				'buying_shares'                => [
-					'class'   => 'container ' . Nh_User::get_user_role() !== Nh_User::INVESTOR ? 'd-none' : '',
+					'class'   => 'container ',
 					'type'    => 'radio',
 					'name'    => 'buying_shares',
 					'title'   => __( '- Investing in companies by entering in a capital increase or buying shares from shareholders?', 'ninja' ),
